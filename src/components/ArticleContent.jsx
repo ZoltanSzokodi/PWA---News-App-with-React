7a18637c7de1067f545696f16ad8410cce034611
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-12-11 12:31:53 +0100

    comment block moved to its own component, plus styled

--- src/components/ArticleContent.jsx
+++ src/components/ArticleContent.jsx
@@ -1,11 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { publishTime, toggleClassesWdarkMode } from '../functions/helpers'
 import { AuthContext } from '../context/AuthContext'
 import { ThemeContext } from '../context/ThemeContext'
-// import { config } from '../firebase'
-import defaultUserImg from '../img/default-profile-picture.jpg'
-import { uuid } from 'uuidv4';
-import firebase from '../firebase'
+import CommentBlock from './CommentBlock'
 import '../styles/ArticleContent.css'
 
 function ArticleContent(props) {
@@ -19,58 +16,6 @@ function ArticleContent(props) {
 
   const { currentUser } = useContext(AuthContext);
   const { darkMode } = useContext(ThemeContext);
-
-  const [comments, setComments] = useState([]);
-  const [newCommentBody, setNewCommentBody] = useState("")
-
-  // Initializing the database 
-  const db = firebase.firestore();
-
-  // ------------------------------------- FETCH ALL COMMENTS --------------------------------
-  const fetchComments = async () => {
-    // if there is no current user do not fetch comments
-    if (currentUser !== null) {
-      const data = await db.collection("comments").get();
-      let commentsArray = [];
-      data.docs.map(doc => commentsArray.push(doc.data()))
-
-      // first I am pushing the comments from the db into an array which I sort according to their time stamp in order to render them in the correct order
-      setComments(commentsArray.sort((a, b) => a.timestamp - b.timestamp));
-    }
-    return;
-  };
-
-  useEffect(() => {
-    fetchComments()
-  }, [])
-
-  // ------------------------------------ UPDATE COMMENTS DB ----------------------------------
-  // sending the new comment to the db
-  const addComment = () => {
-    const comment = {
-      timestamp: Date.now(),
-      article_title: title,
-      comment_body: newCommentBody,
-      user_id: currentUser.uid,
-      user_name: currentUser.displayName
-    }
-
-    db.collection("comments").add(comment)
-      .catch(error => console.log(error))
-
-    // after every comment submit we fetch the comments again
-    fetchComments()
-  }
-
-  // listenig to textarea changes
-  const handleChange = e => {
-    setNewCommentBody(e.target.value)
-  }
-
-  console.log("SELECTED-ARTICLE: ", props.selectedArticle)
-  console.log("CURRENT USER: ", currentUser)
-  console.log("COMMENTS IN DB: ", comments)
-  console.log("NEW COMMENT BODY: ", newCommentBody)
 
   return (
     <div className={toggleClassesWdarkMode(darkMode, "article__content")}>
@@ -225,32 +170,8 @@ function ArticleContent(props) {
 
       <button onClick={props.handleGoBack}>Go Back</button>
 
-      <div className="article__comments--input-container">
-        <h3 className="comment-title">COMMENTS</h3>
-        <textarea className="comment-input-body" name="comment" onChange={handleChange} required></textarea>
-        <button className="comment-btn" onClick={addComment} disabled={newCommentBody === "" && true}>add comment</button>
-      </div>
+      <CommentBlock selectedArticle={props.selectedArticle} />
 
-      <div className="article__comments--output-container">
-        <ul className="comments-list">
-          {comments.map(comment => (
-            (comment.article_title === title) &&
-            <li className="comment" key={uuid()}>
-              <div className="comment__credentials">
-                <img className="comment__credentials--img" src={defaultUserImg} alt="photo" />
-                <span className="comment__credentials--name">{comment.user_name}</span>
-              </div>
-
-              <div className="comment-output-body">{comment.comment_body}</div>
-
-              <div className="comment__change">
-                <i className="fas fa-pen"></i>
-                <i className="fas fa-trash"></i>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
---
+++ src/components/CommentBlock.jsx
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useContext } from 'react'
+import defaultUserImg from '../img/default-profile-picture.jpg'
+import { toggleClassesWdarkMode } from '../functions/helpers'
+import { uuid } from 'uuidv4';
+import { AuthContext } from '../context/AuthContext'
+import { ThemeContext } from '../context/ThemeContext'
+import firebase from '../firebase'
+import '../styles/CommentBlock.css'
+
+
+function CommentBlock(props) {
+  const { title } = props.selectedArticle;
+
+  const { currentUser } = useContext(AuthContext);
+  const { darkMode } = useContext(ThemeContext);
+
+  const [comments, setComments] = useState([]);
+  const [newCommentBody, setNewCommentBody] = useState("");
+
+  // Initializing the database 
+  const db = firebase.firestore();
+
+  // ------------------------------------- FETCH ALL COMMENTS --------------------------------
+  const fetchComments = async () => {
+    // if there is no current user do not fetch comments
+    if (currentUser !== null) {
+      const data = await db.collection("comments").get();
+      let commentsArray = [];
+      data.docs.map(doc => commentsArray.push(doc.data()))
+
+      // first I am pushing the comments from the db into an array which I sort according to their time stamp in order to render them in the correct order
+      setComments(commentsArray.sort((a, b) => a.timestamp - b.timestamp));
+    }
+    return;
+  };
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
+
+  // ------------------------------------ UPDATE COMMENTS DB ----------------------------------
+  // sending the new comment to the db
+  const addComment = () => {
+    const commentArea = document.querySelector(".comment-input-body");
+    const comment = {
+      timestamp: Date.now(),
+      article_title: title,
+      comment_body: newCommentBody,
+      user_id: currentUser.uid,
+      user_name: currentUser.displayName
+    }
+
+    db.collection("comments").add(comment)
+      .catch(error => console.log(error))
+
+    // after every comment submit we fetch the comments again
+    fetchComments()
+    commentArea.value = ""
+  }
+
+  // listenig to textarea changes
+  const handleChange = e => {
+    setNewCommentBody(e.target.value)
+  }
+
+  console.log("SELECTED-ARTICLE: ", props.selectedArticle)
+  console.log("CURRENT USER: ", currentUser)
+  console.log("COMMENTS IN DB: ", comments)
+  console.log("NEW COMMENT BODY: ", newCommentBody)
+
+  return (
+    <React.Fragment>
+      <div className="comments__input-container">
+        <h3 className="comment-title">COMMENTS</h3>
+        <textarea className="comment-input-body" name="comment" wrap="hard" onChange={handleChange} required></textarea>
+        <button className="comment-btn" onClick={addComment} disabled={newCommentBody === "" && true}>add comment</button>
+      </div>
+
+      <div className="comments__output-container">
+        <ul className="comments-list">
+          {comments.map(comment => (
+            (comment.article_title === title) &&
+            <li className="comment" key={uuid()}>
+              <div className="comment__credentials">
+                <img className="comment__credentials--img" src={defaultUserImg} alt="photo" />
+                <span className="comment__credentials--name">{comment.user_name}</span>
+              </div>
+
+              <div className="comment-output-body">{comment.comment_body}</div>
+
+              <div className="comment__change">
+                <i className="fas fa-pen"></i>
+                <i className="fas fa-trash"></i>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default CommentBlock;
